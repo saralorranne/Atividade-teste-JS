a95c1f416386f72cc4d3b52acfdbbafbf19e73e7
saralorranne <sara.l.lima@aluno.senai.br>
2024-01-29 17:07:40 -0300

    Teste 01.js

--- TESTE 01.js
+++ TESTE 01.js
@@ -1,22 +1,23 @@
+let nota = prompt ("Insira uma nota de 0 a 100");
 switch (true) {
     case nota >= 90 && nota <= 100:
-        conceito = "Conceito A";
+        Conceito = "A";
         break;
-    case nota >= 80 && nota <= 89:
-        conceito = "Conceito B";
+    case nota >= 80 && nota <= 90:
+        Conceito = "B";
         break;
-    case nota >= 70 && nota <= 79:
-        conceito = "Conceito C";
+    case nota >= 70 && nota <= 80:
+        Conceito = "c";
         break;
-    case nota >= 60 && nota <= 69:
-        conceito = "Conceito D";
+    case nota >= 60 && nota <= 70:
+        Conceito = "D";
         break;
-    case nota >= 0 && nota <= 59:
-        conceito = "Conceito F";
+    case nota >= 0 && nota <= 60:
+       Conceito =  "F";
         break;
     default:
         conceito = "Nota inválida";
 }
 
-console.log("Conceito: " + conceito);
+console.log("Conceito:" + conceito);
 
